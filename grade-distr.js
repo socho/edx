12aefb499f14b266997d238af9a61a951df18b92
Michelle Johnson <mchlljy@mit.edu>
2013-08-21 15:29:22 -0400

    now buttons become inactive when at the end or beginning of quizzes

--- grade-distr.js
+++ grade-distr.js
@@ -1347,8 +1347,11 @@ var gradeDistr = (function() {
 
         dropdown.find('li').each(function() {
             $(this).on('click', function() {
+
                 //inNav = true;
                 var quizname = String($(this).attr('id'));
+
+                
 
                 $('#asgn-nav').html(quizname+"<span class='caret'></span>");
                 if (quizname != "ViewAll") {
@@ -1377,6 +1380,21 @@ var gradeDistr = (function() {
                         }
                         controller.updateAvrScatterPlot(quizname,"#column1",avr_outerWidth,avr_outerHeight,avr_margin,false);
                     }
+
+                    var listOfQuizzes = controller.getQuizzesArray();
+                    if (quizname == listOfQuizzes[0]) {
+                        $('.btn-l').attr("disabled", true);
+                        $('.btn-r').attr("disabled", false);
+                    }
+                    else if (quizname == listOfQuizzes[listOfQuizzes.length-1]){
+                        $('.btn-r').attr("disabled", true);
+                        $('.btn-l').attr("disabled", false);
+                    }
+                    else {
+                        //renable the buttons
+                        $('.btn-l').attr("disabled", false);
+                        $('.btn-r').attr("disabled", false);
+                    }
                 }
                 else { //when clicks View All
                     $('.btn-l').attr("disabled", true);
@@ -1393,10 +1411,20 @@ var gradeDistr = (function() {
                 }
             });
         });
+        
+        $('.btn-l').attr("disabled", true); //initial button state;
 
         $('.btn-l').on('click', function(){
             var quizzesArray = controller.getQuizzesArray();
             var index = quizzesArray.indexOf($('#asgn-nav').text());
+            if ($('#asgn-nav').text() == quizzesArray[1]) {
+                $(this).attr("disabled", true);
+                $('.btn-r').attr("disabled", false);
+            } else {
+                $(this).attr("disabled", false);
+                $('.btn-r').attr("disabled", false);
+
+            }
             if (index != 0){
                 var quizname = quizzesArray[index-1];
                 $('#asgn-nav').html(quizname+"<span class='caret'></span>");
@@ -1419,6 +1447,13 @@ var gradeDistr = (function() {
         $('.btn-r').on('click', function(){
             var quizzesArray = controller.getQuizzesArray();
             var index = quizzesArray.indexOf($('#asgn-nav').text());
+            if ($('#asgn-nav').text() == quizzesArray[quizzesArray.length-2]) {
+                $(this).attr("disabled", true);
+                $('.btn-l').attr("disabled", false);
+            } else {
+                $(this).attr("disabled", false);
+                $('.btn-l').attr("disabled", false);
+            }
             if (index != quizzesArray.length-1){
                 var quizname = quizzesArray[index+1];
                 $('#asgn-nav').html(quizname+"<span class='caret'></span>");
@@ -1436,7 +1471,9 @@ var gradeDistr = (function() {
                     controller.updateAvrScatterPlot(quizname,"#column1",avr_outerWidth,avr_outerHeight,avr_margin,false);
                 }
             }
+
         });
+
 
 
         model.on('changed', function(data) {
